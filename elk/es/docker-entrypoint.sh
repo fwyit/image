19bd74474da908f9dd8967e97fffbe577ea2db8d
Jam <jianliu@newbanker.cn>
2019-01-16 18:06:14 +0800

    fix 修复mktemp错误

--- elk/es/docker-entrypoint.sh
+++ elk/es/docker-entrypoint.sh
@@ -7,12 +7,17 @@ echo "Elasticsearch User: ${ES_USER:=elasticsearch}"
 cd $ES_HOME
 test -d ${CONF_DIR:=/config} && cp -rf $CONF_DIR/* $ES_HOME/config
 
+### fix mktemp error
+envConf=$ES_HOME/config/elasticsearch-env
+test "$(grep mktemp $envConf | grep 'XXX' | wc -l)" -lt 2 && \
+    sed -i 's/-t elasticsearch/&.XXXXXX/g' $envConf
+
 if [ "${1:0:1}" = '-' ]; then
     set -- elasticsearch "$@"
 fi
 
 if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
-    for path in data logs ; do
+    for path in data logs config; do
         chown -R $ES_USER:$ES_USER "$path"
     done
     
