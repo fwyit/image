dfd2f81b2c7657014f1b265b22ffc93ff5109558
Jam <jianliu@newbanker.cn>
2019-04-17 19:18:05 +0800

    fix 修复无法访问php脚本的bug

--- php/init.sh
+++ php/init.sh
@@ -3,4 +3,6 @@
 #version     : 1.0
 #description : 本脚本主要用来启动php-fpm
 
+LISTEN=${LISTEN:=0.0.0.0}
+sed -i "s/^listen.*/listen = $LISTEN:9000/g" /etc/php7/php-fpm.d/www.conf
 php-fpm7 -t && php-fpm7 -FO
