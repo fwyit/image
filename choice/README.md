43dcd506908b020920b27a953850252de54a60f1
Jam <jianliu@newbanker.cn>
2019-04-17 10:43:07 +0800

    opt 尝试将基础组件放置在base目录中

--- choice/README.md
+++
@@ -1,3 +0,0 @@
-此项目为东方Choice的接口
-
-包下载地址 http://quantapi.eastmoney.com/Download
--- testlink/init.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-#author      : Jam < liujianhncn@gmail.com >
-#version     : 1.0
-#description : 本脚本主要用来启动
